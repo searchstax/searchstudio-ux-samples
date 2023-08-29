cb7e593a2ceb0b981e7bcd550648b41b9a90ed94
Rahul Thapar <rahulthapar.me@gmail.com>
2023-08-29 05:23:05 -0500

    STUDIO-1958: VUE Sample Pages
    Implementation of Sample 1: Searchstax accelerator page in VUE

---
+++ pages/vue/searchstax-accelerator-page/src/widgets/SearchFeedbackWidget.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <SearchstaxSearchFeedbackWidget>
+            <template #searchFeedback="{ searchFeedbackData, onOriginalQueryClick }">
+              <div
+                class="searchstax-feedback-container"
+                v-if="searchFeedbackData && searchFeedbackData?.searchExecuted && searchFeedbackData?.totalResults"
+              > Showing <b>{{ searchFeedbackData.startResultIndex }} - {{ searchFeedbackData.endResultIndex }}</b> of
+                <b>{{ searchFeedbackData.totalResults }}</b> results <span v-if="searchFeedbackData.searchTerm">for "<b>{{
+                  searchFeedbackData.searchTerm }}</b>" </span>
+                <div class="searchstax-feedback-container-suggested">
+                  <div v-if="searchFeedbackData.autoCorrectedQuery"> Search instead for <a
+                      href="#"
+                      @click.prevent="onOriginalQueryClick($event)"
+                      class="searchstax-feedback-original-query"
+                    >{{ searchFeedbackData.originalQuery }}</a>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </SearchstaxSearchFeedbackWidget>
+  </div>
+</template>
+
+<script>
+import { SearchstaxSearchFeedbackWidget } from '@searchstax-inc/searchstudio-ux-vue';
+export default {
+  components: {
+    SearchstaxSearchFeedbackWidget
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ pages/vue/searchstax-accelerator-page/src/widgets/SortingWidget.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <SearchstaxSortingWidget>
+            <template #sorting="{ sortingData, orderChange, selectedSorting }">
+              <div
+                class="searchstax-sorting-container"
+                v-if="sortingData && sortingData?.searchExecuted && sortingData?.hasResultsOrExternalPromotions"
+              >
+                <label
+                  class="searchstax-sorting-label"
+                  for="sort-by"
+                >Sort By</label>
+                <select
+                  id="searchstax-search-order-select"
+                  class="searchstax-search-order-select"
+                  :value="selectedSorting"
+                  @change="orderChange($event)"
+                >
+                  <option value=""> Relevance </option>
+                  <option value="date desc"> Newest Content </option>
+                  <option value="date asc"> Oldest Content </option>
+                </select>
+              </div>
+            </template>
+          </SearchstaxSortingWidget>
+  </div>
+</template>
+
+<script>
+import { SearchstaxSortingWidget } from '@searchstax-inc/searchstudio-ux-vue';
+export default {
+  components: {
+    SearchstaxSortingWidget
+  }
+}
+</script>
+
+<style scoped></style>
