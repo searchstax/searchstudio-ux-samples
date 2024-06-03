a6d78c2c9e251d4a07c110203c45a9424826b6e9
Arminas Matrosovas <a.matrosovas@gmail.com>
2024-06-03 14:03:16 +0300

    react update samples page

--- pages/react/searchstax-accelerator-react/src/App.tsx
+++ pages/react/searchstax-accelerator-react/src/App.tsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
 import "./App.scss";
-import { SearchstaxWrapper } from "@searchstax-inc/searchstudio-ux-react";
-import  InputWidget from './widgets/SearchstaxInputWidget.tsx';
-import ResultWidget from "./widgets/SearchstaxResultWidget.tsx";
+import {
+  SearchstaxWrapper,
+  SearchstaxInputWidget,
+  SearchstaxResultWidget,
+  SearchstaxPaginationWidget,
+  SearchstaxOverviewWidget,
+  SearchstaxSortingWidget,
+  SearchstaxRelatedSearchesWidget,
+  SearchstaxExternalPromotionsWidget,
+  SearchstaxFacetsWidget,
+} from "@searchstax-inc/searchstudio-ux-react";
 
 import type {
   ISearchObject,
   ISearchstaxParsedResult,
-
+  ISearchstaxSuggestProps,
+  ISearchstaxSuggestResponse,
 } from "@searchstax-inc/searchstudio-ux-js";
 import { Searchstax } from "@searchstax-inc/searchstudio-ux-js";
 //@ts-ignore
-import { config }  from '../../config.js';
-import PaginationWidget from "./widgets/SearchstaxPaginationWidget.tsx";
-import OverviewWidget from "./widgets/SearchstaxOverviewWidget.tsx";
-import SortingWidget from "./widgets/SearchstaxSortingWidget.tsx";
-import RelatedSearchesWidget from "./widgets/SearchstaxRelatedSearchesWidget.tsx";
-import ExternalPromotionsWidget from "./widgets/SearchstaxExternalPromotionsWidget.tsx";
-import FacetsWidget from "./widgets/SearchstaxFacetsWidget.tsx";
+import { config } from "../../config.js";
+import { noResultTemplate, resultsTemplate } from "./templates/resultsTemplates.js";
+import { infiniteScrollTemplate, paginationTemplate } from "./templates/paginationTemplates.js";
+import { searchRelatedSearchesTemplate } from "./templates/relatedSearchesTemplates.js";
+import { searchExternalPromotionsTemplate } from "./templates/externalPromotionsTemplates.js";
+import { facetsTemplateDesktop, facetsTemplateMobile } from "./templates/facetTemplates.js";
+import { searchSortingTemplate } from "./templates/sorting.templates.js";
+import { searchOverviewTemplate } from "./templates/searchOverviewTemplates.js";
+import { InputTemplate } from "./templates/inputTemplates.js";
 
 function App() {
   //@ts-ignore
-  const [searchstaxInstance, setSearchstaxInstance] = useState(null as null | Searchstax);
+  const [searchstaxInstance, setSearchstaxInstance] = useState(
+    null as null | Searchstax
+  );
 
   function beforeSearch(props: ISearchObject) {
     const propsCopy = { ...props };
@@ -36,6 +49,24 @@ function App() {
     setSearchstaxInstance(searchstax);
   }
 
+  function afterAutosuggest(result: ISearchstaxSuggestResponse) {
+    const copy = { ...result };
+    return copy;
+  }
+  function beforeAutosuggest(props: ISearchstaxSuggestProps) {
+    // gets suggestProps, if passed along further autosuggest will execute, if null then event gets canceled
+    // props can be modified and passed along
+    const propsCopy = { ...props };
+    return propsCopy;
+  }
+
+  function afterLinkClick(result: ISearchstaxParsedResult) {
+    // gets result that was clicked, if passed along further functions will execute, if null then event gets canceled
+    const resultCopy = { ...result };
+
+    return resultCopy;
+  }
+
   return (
     <>
       <SearchstaxWrapper
@@ -46,28 +77,60 @@ function App() {
         initialized={initialized}
         beforeSearch={beforeSearch}
         afterSearch={afterSearch}
-        authType={config.authType}
+        authType="token"
         router={{ enabled: true }}
-        language={config.language}
+        language="en"
       >
         <div className="searchstax-page-layout-container">
-          <InputWidget />
+          <SearchstaxInputWidget
+            inputTemplate={InputTemplate}
+            afterAutosuggest={afterAutosuggest}
+            beforeAutosuggest={beforeAutosuggest}
+          ></SearchstaxInputWidget>
 
           <div className="search-details-container">
-            <OverviewWidget />
-            <SortingWidget />
+            <SearchstaxOverviewWidget
+              searchOverviewTemplate={searchOverviewTemplate}
+            ></SearchstaxOverviewWidget>
+            <SearchstaxSortingWidget
+              searchSortingTemplate={searchSortingTemplate}
+            ></SearchstaxSortingWidget>
           </div>
 
           <div className="searchstax-page-layout-facet-result-container">
             <div className="searchstax-page-layout-facet-container">
-              <FacetsWidget />
+              <SearchstaxFacetsWidget
+                facetingType="and"
+                itemsPerPageDesktop={2}
+                itemsPerPageMobile={3}
+                specificFacets={undefined}
+                facetsTemplateDesktop={facetsTemplateDesktop}
+                facetsTemplateMobile={facetsTemplateMobile}
+              ></SearchstaxFacetsWidget>
             </div>
 
             <div className="searchstax-page-layout-result-container">
-            <ResultWidget />
-              <ExternalPromotionsWidget />
-              <RelatedSearchesWidget />
-              <PaginationWidget />
+              <SearchstaxExternalPromotionsWidget
+                searchExternalPromotionsTemplate={
+                  searchExternalPromotionsTemplate
+                }
+              ></SearchstaxExternalPromotionsWidget>
+              <SearchstaxResultWidget
+                afterLinkClick={afterLinkClick}
+                resultsPerPage={3}
+                // renderMethod="infiniteScroll"
+                noResultTemplate={noResultTemplate}
+                resultsTemplate={resultsTemplate}
+              ></SearchstaxResultWidget>
+              <SearchstaxRelatedSearchesWidget
+                relatedSearchesURL={config.relatedSearchesURL}
+                relatedSearchesAPIKey={config.relatedSearchesAPIKey}
+                searchRelatedSearchesTemplate={searchRelatedSearchesTemplate}
+              ></SearchstaxRelatedSearchesWidget>
+              <SearchstaxPaginationWidget
+                infiniteScrollTemplate={infiniteScrollTemplate}
+                paginationTemplate={paginationTemplate}
+              ></SearchstaxPaginationWidget>
             </div>
           </div>
         </div>
---
+++ pages/react/searchstax-accelerator-react/src/templates/relatedSearchesTemplates.tsx
@@ -0,0 +1,39 @@
+import { ISearchstaxRelatedSearchResult, ISearchstaxRelatedSearchesData } from "@searchstax-inc/searchstudio-ux-js";
+
+export function searchRelatedSearchesTemplate(
+    relatedData: null | ISearchstaxRelatedSearchesData,
+    executeSearch: (result: ISearchstaxRelatedSearchResult) => void
+  ) {
+    return (
+      <>
+        {relatedData &&
+          relatedData?.searchExecuted &&
+          relatedData?.hasRelatedSearches && (
+            <div
+              className="searchstax-related-searches-container"
+              id="searchstax-related-searches-container"
+            >
+              {" "}
+              Related searches: <span id="searchstax-related-searches"></span>
+              {relatedData.relatedSearches && (
+                <span className="searchstax-related-search">
+                  {relatedData.relatedSearches.map((related) => (
+                    <span
+                      key={related.related_search}
+                      onClick={() => {
+                        executeSearch(related);
+                      }}
+                      className="searchstax-related-search searchstax-related-search-item"
+                    >
+                      {" "}
+                      {related.related_search}
+                      {!related.last && <span>,</span>}
+                    </span>
+                  ))}
+                </span>
+              )}
+            </div>
+          )}
+      </>
+    );
+  }
