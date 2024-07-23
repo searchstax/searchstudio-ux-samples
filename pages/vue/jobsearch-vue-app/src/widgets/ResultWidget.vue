87f8df9b3b20a3ad703d79270b0fb5addb6fdf1e
Arminas Matrosovas <a.matrosovas@gmail.com>
2024-07-23 16:58:05 +0300

    change template

--- pages/vue/jobsearch-vue-app/src/widgets/ResultWidget.vue
+++ pages/vue/jobsearch-vue-app/src/widgets/ResultWidget.vue
@@ -2,56 +2,82 @@
   <div>
     <SearchstaxResultWidget>
       <template #results="{ searchResults, resultClicked }">
-        <div class="searchstax-search-results" v-if="searchResults && searchResults.length">
-          <div
-            class="searchstax-search-result"
-            :class="{ 'has-thumbnail': searchResult.thumbnail, promoted: searchResult.promoted }"
+        <div
+          class="searchstax-search-results"
+          v-if="searchResults && searchResults.length"
+        >
+          <a
             :key="searchResult.uniqueId"
             v-for="searchResult in searchResults"
+            :href="searchResult.url"
+            :data-searchstax-unique-result-id="searchResult.uniqueId"
             @click="resultClicked(searchResult, $event)"
+            class="searchstax-result-item-link searchstax-result-item-link-wrapping"
+            tabindex="0"
           >
-            <div class="searchstax-search-result-content">
-              <div class="searchstax-search-result-title-wrapper">
-                <div class="searchstax-search-result-title-container">
-                  <h3 class="searchstax-search-result-title">{{ searchResult.title }}</h3>
-                </div>
-
-                <div v-if="searchResult.ribbon" class="searchstax-search-result-ribbon">
-                  <span class="pill">{{ searchResult.ribbon }}</span>
-                  <div v-if="searchResult.promoted" class="searchstax-search-result-promoted"></div>
-                </div>
+            <div
+              class="searchstax-search-result"
+              :class="{
+                'has-thumbnail': searchResult.thumbnail,
+                promoted: searchResult.promoted,
+              }"
+            >
+              <div
+                v-if="searchResult.promoted"
+                class="searchstax-search-result-promoted"
+              ></div>
+              <div
+                v-if="searchResult.ribbon"
+                class="searchstax-search-result-ribbon"
+              >
+                {{ searchResult.ribbon }}
               </div>
+              <img
+                v-if="searchResult.thumbnail"
+                :src="searchResult.thumbnail"
+                class="searchstax-thumbnail"
+              />
+              <div class="searchstax-search-result-title-container">
+                <span class="searchstax-search-result-title">{{
+                  searchResult.title
+                }}</span>
+              </div>
+              <p
+                v-if="searchResult.paths"
+                class="searchstax-search-result-common"
+              >
+                {{ searchResult.paths }}
+              </p>
               <p
                 v-if="searchResult.description"
                 class="searchstax-search-result-description searchstax-search-result-common"
               >
-                <b>Description:</b> <br />
                 {{ searchResult.description }}
               </p>
-              <b class="date">{{ searchResult.date }}</b>
-
-              <div :key="unmappedField.key" v-for="unmappedField in searchResult.unmappedFields">
+              <div
+                :key="unmappedField.key"
+                v-for="unmappedField in searchResult.unmappedFields"
+              >
                 <div
-                  v-if="unmappedField.isImage && typeof unmappedField.value === 'string'"
+                  v-if="
+                    unmappedField.isImage &&
+                    typeof unmappedField.value === 'string'
+                  "
                   class="searchstax-search-result-image-container"
                 >
-                  <img :src="unmappedField.value" class="searchstax-result-image" />
+                  <img
+                    :src="unmappedField.value"
+                    class="searchstax-result-image"
+                  />
                 </div>
-                <div class="skills" v-else>
-                  <label>Skills required:</label>
+                <div v-else>
                   <p class="searchstax-search-result-common">
                     {{ unmappedField.value }}
                   </p>
                 </div>
               </div>
             </div>
-
-            <div>
-              <button v-if="searchResult.url" class="btn-primary apply-btn">Apply</button>
-            </div>
-
-            
-          </div>
+          </a>
         </div>
       </template>
     </SearchstaxResultWidget>
@@ -59,21 +85,21 @@
 </template>
 
 <script>
-import { SearchstaxResultWidget } from '@searchstax-inc/searchstudio-ux-vue'
+import { SearchstaxResultWidget } from "@searchstax-inc/searchstudio-ux-vue";
 
 export default {
   data() {
-    return {}
+    return {};
   },
   methods: {
     skillValues(values) {
-      return values.length > 200 ? 'See Job Description' : values
-    }
+      return values.length > 200 ? "See Job Description" : values;
+    },
   },
   components: {
-    SearchstaxResultWidget
-  }
-}
+    SearchstaxResultWidget,
+  },
+};
 </script>
 
 <style scoped>
