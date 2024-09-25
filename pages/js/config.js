b2ccd7b2df3b5c8919790f4b850ef95e1f57a541
Arminas Matrosovas <a.matrosovas@gmail.com>
2024-09-25 16:56:30 +0300

    Revert "Revert "Revert "Update config.js for staging env"""
    This reverts commit 7c8de3f49a14095fdec5d17b5fade5409a534776.

--- pages/js/config.js
+++ pages/js/config.js
@@ -2,38 +2,38 @@ const initConfig = {
   acceleratorSample: {
     language: "en",
     searchURL:
-      "https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182/emselect",
+      "https://searchcloud-2-us-east-1.searchstax.com/29847/corpsiteuxsamples-1442/emselect",
     suggesterURL:
-      "https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182-suggester/emsuggest",
-    searchAuth: "YXBwMzE4Mi1hcGk6U29sbmNlXzIy",
-    trackApiKey: "feqWatajyMvdrpvMyFIMwdHtRdkjPqqHMttUCnVpRss",
-    authType: "basic",
+      "https://searchcloud-2-us-east-1.searchstax.com/29847/corpsiteuxsamples-1442_suggester/emsuggest",
+    searchAuth: "b065448ad1484e205f4851f0ce89d128e704e2f4",
+    trackApiKey: "DPAOKNB9c5chZZDwN1Il9dLUCLMGF1ggehy0dWewZwk",
+    authType: "token",
     relatedSearchesURL:
-      "https://staging.searchstax.co/api/v1/3182/related-search/",
-    relatedSearchesAPIKey: "055651fc679c8492ab98badd08ee6c6f2d8e25ce",
-    analyticsBaseUrl: 'https://analytics-us-west-staging.searchstax.co'
+      "https://app.searchstax.com/api/v1/1447/related-search/",
+    relatedSearchesAPIKey: "3014f9d0276906ac17fff16c500e5ee75b20d73c",
+    analyticsBaseUrl: 'https://analytics-us.searchstax.com'
   },
   acceleratorRelatedSearchSample: {
     relatedSearchesURL:
-      "https://staging.searchstax.co/api/v1/3182/related-search/",
-    relatedSearchesAPIKey: "055651fc679c8492ab98badd08ee6c6f2d8e25ce",
+      "https://app.searchstax.com/api/v1/1442/related-search/",
+    relatedSearchesAPIKey: "fac98ad405cc50e0c0693331e8d2119de592f0e3",
   },
   jobSearchSample: {
     language: "en",
     searchURL:
-      "https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182/emselect",
+      "https://searchcloud-2-us-east-1.searchstax.com/29847/jobsearchuxsample-1447/emselect",
     suggesterURL:
-      "https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182-suggester/emsuggest",
-    searchAuth: "YXBwMzE4Mi1hcGk6U29sbmNlXzIy",
-    trackApiKey: "feqWatajyMvdrpvMyFIMwdHtRdkjPqqHMttUCnVpRss",
-    authType: "basic",
-    analyticsBaseUrl: 'https://analytics-us-west-staging.searchstax.c'
+      "https://searchcloud-2-us-east-1.searchstax.com/29847/jobsearchuxsample-1447_suggester/emsuggest",
+    searchAuth: "9c77f740418413b6701fe7a98cc7c640987a9af1",
+    trackApiKey: "Cnpe2IL7uXaztPzavTIuDAiAmfydOa12f0iAGE0hLlY",
+    authType: "token",
+    analyticsBaseUrl: 'https://analytics-us.searchstax.com'
   },
   jobSearchRelatedSearchSample: {
     relatedSearchesURL:
-      "https://staging.searchstax.co/api/v1/3182/related-search/",
-    relatedSearchesAPIKey: "055651fc679c8492ab98badd08ee6c6f2d8e25ce",
-    analyticsBaseUrl: 'https://analytics-us-west-staging.searchstax.c'
+      "https://app.searchstax.com/api/v1/1447/related-search/",
+    relatedSearchesAPIKey: "3014f9d0276906ac17fff16c500e5ee75b20d73c",
+    analyticsBaseUrl: 'https://analytics-us.searchstax.com'
   },
 };
 
--- pages/react/config.js
+++ pages/react/config.js
@@ -1,13 +1,13 @@
 const config = {
     language: 'en',
-    searchURL: 'https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182/emselect',
-    suggesterURL: 'https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182-suggester/emsuggest',
-    trackApiKey: 'feqWatajyMvdrpvMyFIMwdHtRdkjPqqHMttUCnVpRss',
-    searchAuth: 'YXBwMzE4Mi1hcGk6U29sbmNlXzIy',
-    authType: 'basic',
-    relatedSearchesURL: "https://staging.searchstax.co/api/v1/3182/related-search/",
-    relatedSearchesAPIKey: "055651fc679c8492ab98badd08ee6c6f2d8e25ce",
-    analyticsBaseUrl: 'https://analytics-us-west-staging.searchstax.co'
+    searchURL: 'https://searchcloud-2-us-east-1.searchstax.com/29847/corpsiteuxsamples-1442/emselect',
+    suggesterURL: 'https://searchcloud-2-us-east-1.searchstax.com/29847/corpsiteuxsamples-1442_suggester/emsuggest',
+    trackApiKey: 'DPAOKNB9c5chZZDwN1Il9dLUCLMGF1ggehy0dWewZwk',
+    searchAuth: 'b065448ad1484e205f4851f0ce89d128e704e2f4',
+    authType: 'token',
+    relatedSearchesURL: "https://app.searchstax.com/api/v1/1442/related-search/",
+    relatedSearchesAPIKey: "fac98ad405cc50e0c0693331e8d2119de592f0e3",
+    analyticsBaseUrl: 'https://analytics-us.searchstax.com'
 };
 
 const renderConfig = {
