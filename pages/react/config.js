71d3da0636db0628b326934a0cb4cf6848047b28
Arminas Matrosovas <a.matrosovas@gmail.com>
2025-02-26 14:37:36 +0800

    unify

--- pages/react/config.js
+++ pages/react/config.js
@@ -4,7 +4,7 @@ const config = {
     suggesterURL: 'https://ss764687-aiypyk27-us-east-2-aws.searchstax.co/solr/accessibilityjuliatestdonotdelete-3182-suggester/emsuggest',
     trackApiKey: 'feqWatajyMvdrpvMyFIMwdHtRdkjPqqHMttUCnVpRss',
     searchAuth: 'YXBwMzE4Mi1hcGk6U29sbmNlXzIy',
-    authType: 'basic',
+    authType: "basic",
     relatedSearchesURL: "https://staging.searchstax.co/api/v1/3182/related-search/",
     relatedSearchesAPIKey: "055651fc679c8492ab98badd08ee6c6f2d8e25ce",
     analyticsBaseUrl: 'https://analytics-us-west-staging.searchstax.co',
