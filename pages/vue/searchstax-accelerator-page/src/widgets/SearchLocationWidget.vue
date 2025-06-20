e04cc371833d3f32b5504df02cbe064e98a7817e
Arminas Matrosovas <a.matrosovas@gmail.com>
2025-06-20 15:36:21 +0300

    location templates

--- pages/vue/searchstax-accelerator-page/src/widgets/SearchLocationWidget.vue
+++ pages/vue/searchstax-accelerator-page/src/widgets/SearchLocationWidget.vue
@@ -1,68 +1,81 @@
 <template>
-    <SearchstaxLocationWidget
-      :locationDecode="renderConfig.locationWidget.locationDecode"
+  <SearchstaxLocationWidget :locationDecode="renderConfig.locationWidget.locationDecode">
+    <template
+      #location="{
+        locationData,
+        inputValue,
+        locationBlur,
+        radiusChange,
+        selectValue,
+        inputChange,
+        locationError,
+        getCurrentLocation
+      }"
     >
-    <template #location="{ locationData, inputValue, locationBlur, radiusChange, selectValue, inputChange, locationError }">
-            <div
-      class="searchstax-location-input-container"
-      data-test-id="searchstax-location-input-container"
-    >
-      <div class="searchstax-location-input-wrapper">
-        <span class="searchstax-location-input-label">NEAR</span>
-        <input
-          type="text"
-          id="searchstax-location-input"
-          class="searchstax-location-input"
-          :class="{'searchstax-input-location-error': locationError}"
-          placeholder="Zip, Postal Code or City..."
-          aria-label="Search Location Input"
-          data-test-id="searchstax-location-input"
-          @change="inputChange"
-          :value="inputValue"
-          @blur="locationBlur"
-        />
-        <span
-          v-if="locationData.shouldShowLocationDistanceDropdown"
-          class="searchstax-location-input-label"
-          >WITHIN</span
-        >
-        <select
-          v-if="locationData.shouldShowLocationDistanceDropdown"
-          id="searchstax-location-radius-select"
-          class="searchstax-location-radius-select"
-          aria-label="Search Location Radius Select"
-          data-test-id="searchstax-location-radius-select"
-          @change="radiusChange"
-          :value="selectValue"
-        >
-          <option
-            v-for="value in locationData.locationSearchDistanceValues"
-            :value="value.value"
-            :key="value.value"
+      <div
+        class="searchstax-location-input-container"
+        data-test-id="searchstax-location-input-container"
+      >
+        <div class="searchstax-location-input-wrapper">
+          <span class="searchstax-location-input-label">NEAR</span>
+          <div class="searchstax-location-input-wrapper-inner">
+            <input
+              type="text"
+              id="searchstax-location-input"
+              class="searchstax-location-input"
+              :class="{ 'searchstax-input-location-error': locationError }"
+              placeholder="Zip, Postal Code or City..."
+              aria-label="Search Location Input"
+              data-test-id="searchstax-location-input"
+              @change="inputChange"
+              :value="inputValue"
+              @blur="locationBlur"
+            />
+            <button @click="getCurrentLocation" class="searchstax-get-current-location-button">
+              Use my current location
+            </button>
+          </div>
+          <span
+            v-if="locationData.shouldShowLocationDistanceDropdown"
+            class="searchstax-location-input-label"
+            >WITHIN</span
           >
-            {{value.label}}
-          </option>
-        </select>
+          <select
+            v-if="locationData.shouldShowLocationDistanceDropdown"
+            id="searchstax-location-radius-select"
+            class="searchstax-location-radius-select"
+            aria-label="Search Location Radius Select"
+            data-test-id="searchstax-location-radius-select"
+            @change="radiusChange"
+            :value="selectValue"
+          >
+            <option
+              v-for="value in locationData.locationSearchDistanceValues"
+              :value="value.value"
+              :key="value.value"
+            >
+              {{ value.label }}
+            </option>
+          </select>
+        </div>
       </div>
-    </div>
-          </template>
-    </SearchstaxLocationWidget>
+    </template>
+  </SearchstaxLocationWidget>
 </template>
 
 <script>
 import { SearchstaxLocationWidget } from '@searchstax-inc/searchstudio-ux-vue'
-import {renderConfig} from '../../../config';
-
+import { renderConfig } from '../../../config'
 
 export default {
   components: {
     SearchstaxLocationWidget
   },
-  data(){
-    return{
+  data() {
+    return {
       renderConfig
     }
-  },
+  }
 }
 </script>
 
