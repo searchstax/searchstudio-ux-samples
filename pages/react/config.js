ea5a9cf51d205aacdc986fbdaf5696be830d1bde
Arminas Matrosovas <a.matrosovas@gmail.com>
2026-01-13 19:57:28 +0200

    update configs

--- pages/react/config.js
+++ pages/react/config.js
@@ -1,20 +1,22 @@
 const config = {
   language: "en",
-  model: "Main Profile",
-  searchURL:
-    "https://searchcloud-django5-staging-27-us-east-1.searchstax.co/138/corpsiteappdonotdelete-4588/emselect",
-  suggesterURL:
-    "https://searchcloud-django5-staging-27-us-east-1.searchstax.co/138/corpsiteappdonotdelete-4588_suggester/emsuggest",
-  trackApiKey: "ynDxQycRAG5NKtztf9LURH15uPEWA8BN7XV24bybe1k",
-  searchAuth: "a1cede71fad5ae41dc8efc99f96c647e3dbdd38d",
-  authType: "token",
-  relatedSearchesURL:
-    "https://staging.searchstax.co/api/v1/4588/related-search/",
-  relatedSearchesAPIKey: "e514efc3776ab13bda2ce5984085010073078009",
-  analyticsBaseUrl: "https://analytics-us-west-staging.searchstax.co",
-  questionURL:
-    "https://search-ai-us-west-staging.searchstax.co/api/v1/4588/answer/",
-  appId: "4588",
+    model: "Main Profile",
+    searchURL:
+      "https://searchcloud-staging-3-us-east-1.searchstax.co/138/corpsitednrcrawlerdnd-6246/emselect",
+    suggesterURL:
+      "https://searchcloud-staging-3-us-east-1.searchstax.co/138/corpsitednrcrawlerdnd-6246_suggester/emsuggest",
+    trackApiKey: "CKHcsBBTMKtH1xWxhw76Tt3bpgbMAbKwlnyHginyoGc",
+    searchAuth: "7e6c816ba15a9f4917046f4d2967eef13d2a5a9f",
+    authType: "token",
+    relatedSearchesURL:
+      "https://staging.searchstax.co/api/v1/6246/related-search/",
+    relatedSearchesAPIKey: "https://staging.searchstax.co/api/v1/6246/related-search/",
+    analyticsBaseUrl: "https://analytics-us-west-staging.searchstax.co",
+    analyticsSrc:
+      "https://static-staging.searchstax.co/studio-js/v4/js/studio-analytics.js",
+    questionURL:
+      "https://search-ai-us-west-staging.searchstax.co/api/v1/6246/answer/",
+    appId: "6246",
 };
 
 const renderConfig = {
