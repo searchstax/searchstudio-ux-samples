0f7ac1546480cf80c5c960596e3b8fae92d339df
Karalevich <karalevichandrei@gmail.com>
2026-05-22 11:28:49 -0700

    STUDIO-5257: JS/VUE update version to 4.2.9 and add clear input button to search input widget

--- pages/vue/searchstax-accelerator-page/src/widgets/SearchInputWidget.vue
+++ pages/vue/searchstax-accelerator-page/src/widgets/SearchInputWidget.vue
@@ -5,7 +5,7 @@
       :beforeAutosuggest="beforeAutosuggest"
       :suggestAfterMinChars="renderConfig.inputWidget.suggestAfterMinChars"
     >
-    <template #input="{ suggestions, onMouseLeave, onMouseOver, onMouseClick }">
+    <template #input="{ suggestions, onMouseLeave, onMouseOver, onMouseClick, onClearClick, hasInputValue }">
             <div class="searchstax-search-input-wrapper">
               <input
                 type="text"
@@ -14,6 +14,8 @@
                 placeholder="SEARCH FOR..."
                 aria-label="search"
               />
+              <button v-if="hasInputValue" @click="onClearClick" @keyup.enter="onClearClick" id="searchstax-clear-input-action-button" class="searchstax-cross-icon " aria-label="clear input" role="button"></button>
+              <span v-if="hasInputValue" id="searchstax-separator-icon" class="searchstax-separator "></span>
               <div
                 class="searchstax-autosuggest-container"
                 :class="{ 'hidden': suggestions.length === 0 }"
